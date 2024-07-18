74e15594b1e1b5f9da8d9824186cd714dcc83f30
PhantomOz <faniogor@gmail.com>
2024-07-18 23:32:35 +0100

    This commit edit verify and compile code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,49 @@ import fsPromises from "fs/promises";
 import { spawn } from "child_process";
 import { Addressable } from "ethers";
 
+export async function compileContract(name: string, contract: string) {
+  name = name.replace(" ", "");
+  (async function main() {
+    try {
+      await fsPromises.writeFile("../../contracts/Lock.sol", contract);
+
+      console.log("File written successfully");
+      console.log("The written file has" + " the following contents:");
+
+      console.log("" + fs.readFileSync("../../contracts/Lock.sol"));
+    } catch (err) {
+      console.error(err);
+    }
+  })();
+
+  const child = spawn("npx hardhat compile", {
+    stdio: "inherit",
+    shell: true,
+  });
+
+  return new Promise<string>((resolve, reject) => {
+    child.on("exit", function (code, signal) {
+      if (code === 0) {
+        try {
+          let compiledContract = fs.readFileSync(
+            `../../artifacts/contracts/Lock.sol/${name}.json`,
+            "utf8"
+          );
+          resolve(compiledContract);
+        } catch (err) {
+          reject(err);
+        }
+      } else {
+        reject(
+          new Error(
+            `child process exited with code ${code} and signal ${signal}`
+          )
+        );
+      }
+    });
+  });
+}
+
 export async function verifyContract(
   contractAddress: string | Addressable,
   contractSourceCode: string,
