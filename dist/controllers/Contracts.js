934297f42f5dc5c0464675029eef0e12f7ef6e29
PhantomOz <faniogor@gmail.com>
2024-07-19 12:50:59 +0100

    Added the build to it

---
+++ dist/controllers/Contracts.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.compile = compile;
+exports.verify = verify;
+const utils_1 = require("../utils");
+async function compile(req, res) {
+    let { name, contract } = req.body;
+    try {
+        const compiledContract = await (0, utils_1.compileContract)(name, contract);
+        res.status(200).json({ data: compiledContract });
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
+}
+async function verify(req, res) {
+    let { contractAddress, contractSourceCode, contractName, constructorArguments, } = req.body;
+    try {
+        const verified = (0, utils_1.verifyContract)(contractAddress, contractSourceCode, contractName, constructorArguments);
+        res.status(200).json({ data: verified });
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
+}
---
+++ dist/utils/index.js
@@ -0,0 +1,66 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.compileContract = compileContract;
+exports.verifyContract = verifyContract;
+const fs_1 = __importDefault(require("fs"));
+const promises_1 = __importDefault(require("fs/promises"));
+const child_process_1 = require("child_process");
+async function compileContract(name, contract) {
+    name = name.replace(" ", "");
+    (async function main() {
+        try {
+            await promises_1.default.writeFile("../../contracts/Lock.sol", contract);
+            console.log("File written successfully");
+            console.log("The written file has" + " the following contents:");
+            console.log("" + fs_1.default.readFileSync("../../contracts/Lock.sol"));
+        }
+        catch (err) {
+            console.error(err);
+        }
+    })();
+    const child = (0, child_process_1.spawn)("npx hardhat compile", {
+        stdio: "inherit",
+        shell: true,
+    });
+    return new Promise((resolve, reject) => {
+        child.on("exit", function (code, signal) {
+            if (code === 0) {
+                try {
+                    let compiledContract = fs_1.default.readFileSync(`../../artifacts/contracts/Lock.sol/${name}.json`, "utf8");
+                    resolve(compiledContract);
+                }
+                catch (err) {
+                    reject(err);
+                }
+            }
+            else {
+                reject(new Error(`child process exited with code ${code} and signal ${signal}`));
+            }
+        });
+    });
+}
+async function verifyContract(contractAddress, contractSourceCode, contractName, constructorArguments) {
+    await compileContract(contractName, contractSourceCode);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const child = (0, child_process_1.spawn)(`npx hardhat verify --network sepolia ${contractAddress} ${constructorArguments?.join(" ")}`, {
+                stdio: "inherit",
+                shell: true,
+            });
+            child.on("exit", function (code, signal) {
+                if (code === 0) {
+                    resolve("done");
+                }
+                else {
+                    reject(new Error(`child process exited with code ${code} and signal ${signal}`));
+                }
+            });
+            child.on("error", (err) => {
+                reject(err);
+            });
+        }, 60000);
+    });
+}
