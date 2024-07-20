3a080d3f382980f6dc249bc7aa1f9c3943aad6e3
PhantomOz <faniogor@gmail.com>
2024-07-20 11:08:49 +0100

    This commit adds ENV for hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `${process.env.SEP_RPC_URL}`,
+    },
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+  },
 };
 
 export default config;
